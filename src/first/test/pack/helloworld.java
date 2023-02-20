707acfc8c0dd16a24d722908c888b785b6c097f0
Eqnxy <r.fadejev@gmail.com>
2023-02-21 01:52:18 +0300

    1. task

---
+++ src/first/test/pack/helloworld.java
@@ -0,0 +1,8 @@
+package first.test.pack;
+public class helloworld {
+    //метод main, с которого начинается выполнение программы
+    public static void main(String[] args) {
+        //выводим значение в ".." в консоль
+        System.out.println("Hello" + " " + "World!");
+    }
+}
