4feed7dc96ac68e8bfae5f8b2e1bdaa1626ffffe
Eqnxy <r.fadejev@gmail.com>
2023-03-22 01:34:18 +0300

    JavaBegginner 4 задание

--- src/Java/Basic/Final4.java
+++ src/Java/Basic/Final4.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 public class Final4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int attemptsLeft = 3;
-        boolean hintUsed = false;
-        String riddle = "Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает";
         String answer = "Заархивированный вирус";
+        System.out.println("Отгадайте загадку: Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        int attempts = 1;
+        boolean hintused = false;
 
-        while (attemptsLeft > 0) {
-            System.out.println("Осталось попыток: " + attemptsLeft);
-            System.out.print("Введите ответ: ");
-            String userInput = scanner.nextLine().trim().toLowerCase();
 
-            if (userInput.equals(answer.toLowerCase())) {
+        while (attempts <= 3) {
+            System.out.println(attempts);
+            String userInput = scanner.nextLine();
+
+
+            if (userInput.equalsIgnoreCase(answer)) {
                 System.out.println("Правильно!");
                 return;
-            } else if (userInput.equals("подсказка") && !hintUsed && attemptsLeft == 3) {
+            } else if (attempts == 1 && userInput.equalsIgnoreCase("подсказка")) {
                 System.out.println("Вирус");
-                hintUsed = true;
-            } else if (userInput.equals("подсказка") && (hintUsed || attemptsLeft < 3)) {
-                System.out.println("Подсказка уже недоступна");
+                attempts++;
+                hintused = true;                
+            } else if (attempts > 1 && userInput.equalsIgnoreCase("подсказка")) {
+                System.out.println("Подсказка недоступна");
+                attempts++;
+            } else if (attempts == 3 || (attempts == 2 && hintused && !userInput.equalsIgnoreCase(answer))) {
+                System.out.println("Обидно, приходи в другой раз");
+                return;
             } else {
-                System.out.println("Подумай еще!");
-                attemptsLeft--;
-                if (attemptsLeft == 0) {
-                    System.out.println("Обидно, приходи в другой раз");
-                }
+                System.out.println("Подумайте еще!");
+                attempts++;
             }
         }
     }
